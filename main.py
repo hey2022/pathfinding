865990d05454ee61f83c218a4b0a1ddb3bc16c44
aulimaru <aulimarutama@proton.me>
2023-12-20 10:09:24 +0800

    add class Matrix and modified Graph to work with it

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 import sys
+from enum import Enum, auto
 
 
 class Graph:
-    def __init__(self, width: int, height: int, block_size: int, grid_thickness: int):
-        self.width = width * block_size
-        self.height = height * block_size
+    def __init__(self, cols: int, rows: int, block_size: int, grid_thickness: int):
+        self.width = cols * block_size
+        self.height = rows * block_size
         self.block_size = block_size
         self.grid_thickness = grid_thickness
         self.surface = pygame.display.set_mode((self.width, self.height))
+        self.matrix = Matrix(cols, rows)
 
-    def create_node(self, x: int, y: int) -> pygame.Rect:
+    def pixel_to_index(self, pos: (int, int)):
+        (x, y) = pos
+        return (x // self.block_size, y // self.block_size)
+
+    def index_to_pixel(self, pos: (int, int)):
+        (x, y) = pos
+        return (x * self.block_size, y * self.block_size)
+
+    def create_node(self, pos: (int, int)) -> pygame.Rect:
+        (x, y) = pos
         return pygame.Rect(
             x // self.block_size * self.block_size,
             y // self.block_size * self.block_size,
@@ -19,96 +30,107 @@ class Graph:
         )
 
     def draw_node(self, color: int, node: pygame.Rect) -> pygame.Rect:
-        if node == self.source:
-            self.source = None
-        if node == self.target:
-            self.target = None
         pygame.draw.rect(
             self.surface,
             color,
-            node,
+            node
         )
         pygame.display.update(node)
         return node
 
-    def add_wall(self, wall: pygame.Rect):
-        if wall not in self.walls:
-            self.walls.append(wall)
+    def add_wall(self, pos):
+        (x, y) = self.pixel_to_index(pos)
+        if self.matrix.source == (x, y):
+            self.matrix.source = None
+        if self.matrix.target == (x, y):
+            self.matrix.targer = None
+        self.matrix.add_wall(self.pixel_to_index(pos))
+        self.draw_node(FOREGROUND_COLOR, self.create_node(pos))
 
-    def add_visited(self, visited: pygame.Rect):
-        if visited not in self.visited:
-            self.visited.append(visited)
-
-    def clear_node(self, node: pygame.Rect):
-        if node == self.source:
-            self.source = None
-        if node == self.target:
-            self.target = None
-        if node in self.walls:
-            self.walls.remove(node)
-        pygame.draw.rect(
-            self.surface,
-            BACKGROUND_COLOR,
-            node,
-        )
-        pygame.draw.rect(self.surface, FOREGROUND_COLOR, node, self.grid_thickness)
-        pygame.display.update(node)
-
-    def update_target(self, node: pygame.Rect):
-        if self.target == node:
+    def update_target(self, pos: (int, int)):
+        (x, y) = self.pixel_to_index(pos)
+        if self.matrix.target == (x, y):
             return
-        if node in self.walls:
-            self.walls.remove(node)
-        if self.target is not None:
-            self.clear_node(self.target)
-        self.target = self.draw_node(0xFF0000, node)
+        self.matrix.walls[x][y] = False
+        if self.matrix.target is not None:
+            self.clear_node(self.index_to_pixel(self.matrix.target))
+        self.matrix.target = (x, y)
+        self.draw_node(0xFF0000, self.create_node(pos))
 
-    def update_source(self, node: pygame.Rect):
-        if self.source == node:
+    def update_source(self, pos: (int, int)):
+        (x, y) = self.pixel_to_index(pos)
+        if self.matrix.source == (x, y):
             return
-        if node in self.walls:
-            self.walls.remove(node)
-        if self.source is not None:
-            self.clear_node(self.source)
-        self.source = self.draw_node(0x99FFCC, node)
+        self.matrix.walls[x][y] = False
+        if self.matrix.source is not None:
+            self.clear_node(self.index_to_pixel(self.matrix.source))
+        self.matrix.source = (x, y)
+        self.draw_node(0x99FFCC, self.create_node(pos))
+
+    def clear_node(self, pos: (int, int)):
+        (x, y) = self.pixel_to_index(pos)
+        if self.matrix.source == (x, y):
+            self.matrix.source = None
+        if self.matrix.target == (x, y):
+            self.matrix.targer = None
+        self.matrix.walls[x][y] = False
+        self.draw_node(BACKGROUND_COLOR, self.create_node(pos))
 
     def clear_board(self):
-        self.source = None
-        self.target = None
-        self.walls = []
-        self.visited = []
+        self.matrix.reset()
         self.surface.fill(BACKGROUND_COLOR)
         pygame.display.update()
-        for x in range(0, self.width, self.block_size):
-            for y in range(0, self.height, self.block_size):
-                node = self.create_node(x, y)
-                pygame.draw.rect(
-                    self.surface, FOREGROUND_COLOR, node, self.grid_thickness
-                )
-        pygame.display.update()
 
-    def is_valid_node(self, node: pygame.Rect):
-        x, y = node.left, node.top
-        in_bounds = x >= 0 and x < self.width and y >= 0 and y < self.height
-        not_wall = node not in self.walls
-        return in_bounds and not_wall
 
-    def dfs(self, clock: pygame.time.Clock, x: int, y: int):
-        clock.tick(120)
-        for direction in DIRECTIONS:
-            new_x = x + direction[0] * self.block_size
-            new_y = y + direction[1] * self.block_size
-            node = self.create_node(new_x, new_y)
-            if node == self.target:
-                return 1
-            if self.is_valid_node(node) and node not in self.visited:
-                self.visited.append(node)
-                self.draw_node(0x99FFCC, node)
-                if self.dfs(clock, new_x, new_y):
-                    self.clear_node(node)
-                    return 1
-                self.clear_node(node)
-        return 0
+class EventType(Enum):
+    VISIT = auto()
+    LEAVE = auto()
+
+
+class Event:
+    def __init__(self, typ: EventType, pos: (int, int)):
+        self.typ = typ
+        self.pos = pos
+
+
+class Matrix:
+    def __init__(self, cols: int, rows: int):
+        self.cols = cols
+        self.rows = rows
+        self.reset()
+
+    def reset(self):
+        self.source = None
+        self.target = None
+        self.init_walls()
+        self.init_visited()
+        self.init_events()
+
+    def init_walls(self):
+        self.walls = [[False for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def init_visited(self):
+        self.visited = [[False for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def init_events(self):
+        self.events = []
+
+    def add_wall(self, pos: (int, int)):
+        (x, y) = pos
+        if self.walls[x][y]:
+            return False
+        self.walls[x][y] = True
+        return True
+
+    def dfs(self, graph: Graph):
+        if self.events.empty():
+            return
+        event = self.events.top()
+        self.events.pop()
+        if event.typ == EventType.LEAVE:
+            pass
+        else:
+            pass
 
 
 def quit():
@@ -141,25 +163,17 @@ def event_handler(
 
     if key_press(event, pygame.K_s):
         x, y = pygame.mouse.get_pos()
-        node = graph.create_node(x, y)
-        graph.update_source(node)
+        graph.update_source((x, y))
 
     if key_press(event, pygame.K_t):
         x, y = pygame.mouse.get_pos()
-        node = graph.create_node(x, y)
-        graph.update_target(node)
+        graph.update_target((x, y))
 
     if key_press(event, pygame.K_c):
         graph.clear_board()
 
     if key_press(event, pygame.K_RETURN):
-        if graph.source is not None and graph.target is not None:
-            graph.visited = [graph.source]
-            graph.dfs(
-                clock,
-                graph.source.left,
-                graph.source.top,
-            )
+        pass
 
 
 def left_mouse_drag() -> bool:
@@ -178,14 +192,11 @@ def main():
     while True:
         if left_mouse_drag():
             x, y = pygame.mouse.get_pos()
-            wall = graph.create_node(x, y)
-            graph.draw_node(FOREGROUND_COLOR, wall)
-            graph.add_wall(wall)
+            graph.add_wall((x, y))
 
         if right_mouse_drag():
             x, y = pygame.mouse.get_pos()
-            node = graph.create_node(x, y)
-            graph.clear_node(node)
+            graph.clear_node((x, y))
 
         for event in pygame.event.get():
             event_handler(graph, surface, clock, event)
