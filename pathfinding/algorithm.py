fd8e0d17bd8f9c4a3ab0ab8260bb9f936c89e1e7
Yiheng He <yiheng.he@proton.me>
2024-01-07 18:21:37 +0800

    Removed old code

--- pathfinding/algorithm.py
+++ pathfinding/algorithm.py
@@ -2,8 +2,6 @@ from pathfinding.graph import *
 from collections import deque
 
 
-# from enum import Enum, auto
-
 DIRECTIONS = [
     (0, -1),
     (1, 0),
@@ -67,86 +65,4 @@ def bfs(graph: Graph) -> Result:
         graph.display_nodes(neighbours)
     return Result([], explored)
 
-    # heapq.heappush(open_set, ((0, 0), start))
-    # came_from = {}
-    # g_score = {start: 0}
-    # f_score = {start: self.h_cost(start, goal)}
-    #     while len(open_set) > 0:
-    #         current = heapq.heappop(open_set)[1]
-    #         if current == goal:
-    #             return self.reconstruct_path(came_from, current)
-    #         neighbors = [(current[0], current[1] - game.block), (current[0] + game.block, current[1]),
-    #                      (current[0], current[1] + game.block), (current[0] - game.block, current[1])]
-    #         for neighbor in neighbors:
-    #             if self.valid(neighbor):
-    #                 new_g = g_score.get(current) + game.block
-    #                 if new_g < g_score.get(neighbor, math.inf):
-    #                     came_from[neighbor] = current
-    #                     g_score[neighbor] = new_g
-    #                     f_score[neighbor] = new_g + self.h_cost(neighbor, goal)
-    #                     if ((f_score[neighbor], self.h_cost(neighbor, goal)), neighbor) not in open_set:
-    #                         heapq.heappush(open_set, ((f_score[neighbor], self.h_cost(neighbor, goal)), neighbor))
 
-
-# def dfs(graph: Graph):
-#     if len(graph.events) == 0 or graph.source is None:
-#         return -1
-#     event = graph.events.pop()
-#     if not graph.avaliable(event.pos) and event.typ == EventType.VISIT:
-#         return 0
-#     if event.typ == EventType.LEAVE:
-#         graph.draw_visited(event.pos)
-#     else:
-#         if event.pos == graph.target:
-#             graph.init_events()
-#             return -1
-#         graph.draw_visiting(event.pos)
-#         graph.events.append(Event(event.pos, EventType.LEAVE))
-#         (x, y) = event.pos
-#         graph.visited[y][x] = True
-#         for dx, dy in DIRECTIONS:
-#             newpos = (x + dx, y + dy)
-#             graph.events.append(Event(newpos, EventType.VISIT))
-#     return 1
-
-
-# class EventType(Enum):
-#     VISIT = auto()
-#     LEAVE = auto()
-
-
-# class Event:
-#     def __init__(self, pos: tuple[int, int], typ: EventType):
-#         self.pos = pos
-#         self.typ = typ
-
-
-# class Matrix:
-#     def __init__(self, columns: int, rows: int):
-#         self.columns = columns
-#         self.rows = rows
-#         self.reset()
-
-#     def reset(self):
-#         self.source = None
-#         self.target = None
-#         self.init_walls()
-#         self.init_visited()
-#         self.init_events()
-
-#     def init_walls(self):
-#         self.walls = [[False for _ in range(self.columns)] for _ in range(self.rows)]
-
-#     def init_visited(self):
-#         self.visited = [[False for _ in range(self.columns)] for _ in range(self.rows)]
-
-#     def init_events(self):
-#         self.events = []
-
-# def draw_visited(self, index_pos):
-#     pos = self.index_to_pixel(index_pos)
-#     self.draw_at_index(0xA7ABC3, pos)
-
-# def draw_visiting(self, index_pos):
-#     pos = self.index_to_pixel(index_pos)
-#     self.draw_at_index(0x99FFCC, pos)
