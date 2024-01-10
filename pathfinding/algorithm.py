hey2022/pathfinding

commit f241966a5e6d12e47679d0a2f97f370dd0f77c79

Made a_star faster

pathfinding/algorithm.py
@@ -59,14 +59,16 @@ def a_star(graph: Graph) -> Result:
     while priority_queue:
         node = heapq.heappop(priority_queue)[1]
         if node == graph.target:
-            return Result(reconstruct_path(came_from[node], came_from), came_from.keys())
-        if node != graph.source:
-            pygame.display.update(graph.draw_node(node, 0x565656))
+            return Result(
+                reconstruct_path(came_from[node], came_from), came_from.keys()
+            )
         neighbours = get_neighbours(node)
         for neighbour in neighbours:
             if valid_node(graph, neighbour):
                 new_cost = cost[node] + 1
                 if new_cost < cost.get(neighbour, math.inf):
+                    if node != graph.source and neighbour not in came_from:
+                        pygame.display.update(graph.draw_node(node, 0x565656))
                     came_from[neighbour] = node
                     cost[neighbour] = new_cost
                     if (

commit cfbb91ed22719fa09c792e735267dbee7042f6bb

Fixed a_star rendering broken from commit f241966

pathfinding/algorithm.py
@@ -53,22 +53,22 @@ def a_star(graph: Graph) -> Result:
     priority_queue = []
     came_from = {}
     cost = {graph.source: 0}
+    explored = set()
     heapq.heappush(
         priority_queue, (heuristic(graph.source, graph.target), graph.source)
     )
     while priority_queue:
         node = heapq.heappop(priority_queue)[1]
         if node == graph.target:
-            return Result(
-                reconstruct_path(came_from[node], came_from), came_from.keys()
-            )
+            return Result(reconstruct_path(came_from[node], came_from), explored)
+        if node != graph.source and node not in explored:
+            explored.add(node)
+            pygame.display.update(graph.draw_node(node, 0x565656))
         neighbours = get_neighbours(node)
         for neighbour in neighbours:
             if valid_node(graph, neighbour):
                 new_cost = cost[node] + 1
                 if new_cost < cost.get(neighbour, math.inf):
-                    if node != graph.source and neighbour not in came_from:
-                        pygame.display.update(graph.draw_node(node, 0x565656))
                     came_from[neighbour] = node
                     cost[neighbour] = new_cost
                     if (